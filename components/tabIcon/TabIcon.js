8410acdfe56c336fde6e1360c96b8a94f9bd0289
dragon <isanoguchi721@gmail.com>
2022-11-14 10:01:40 -0800

    tabicon

--- components/tabIcon/TabIcon.js
+++ components/tabIcon/TabIcon.js
@@ -1,16 +1,38 @@
 import Image from "next/image";
+import { useState } from "react";
 
 const TabIcon = ({ imgName, name }) => {
+  const [hover, setHover] = useState(false);
+
   return (
-    <div className="w-[108px] text-center pb-[12px] pt-[21px] cursor-pointer hover:bg-[#545AF2]">
-      <Image
-        className="m-auto "
-        src={`/svg/${imgName}`}
-        alt="me"
-        width="28"
-        height="28"
-      />
-      <p className="tabIcon-name">{name}</p>
+    <div
+      className="w-[108px] text-center pb-[12px] pt-[21px] cursor-pointer shadow-inner hover:border-t-4 border-[#545AF2]"
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
+    >
+      {hover ? (
+        <>
+          <Image
+            className="m-auto "
+            src={`/svg/${imgName}hover.svg`}
+            alt="me"
+            width="28"
+            height="28"
+          />
+          <p className="font-[Poppins] text-[12px] text-[#545AF2]">{name}</p>
+        </>
+      ) : (
+        <>
+          <Image
+            className="m-auto "
+            src={`/svg/${imgName}.svg`}
+            alt="me"
+            width="28"
+            height="28"
+          />
+          <p className="font-[Poppins] text-[12px]">{name}</p>
+        </>
+      )}
     </div>
   );
 };
--- components/tabMenu/TabMenu.js
+++ components/tabMenu/TabMenu.js
@@ -3,12 +3,12 @@ import TabIcon from "../tabIcon/TabIcon";
 const TabMenu = () => {
   return (
     <div className="flex bg-[white] mt-[24px]">
-      <TabIcon imgName="dashboard.svg" name="Feed" />
-      <TabIcon imgName="friends.svg" name="My Networks" />
-      <TabIcon imgName="Group.svg" name="Dating" />
-      <TabIcon imgName="marketplace.svg" name="Marketplace" />
-      <TabIcon imgName="portfolio.svg" name="Jobs" />
-      <TabIcon imgName="calendar.svg" name="Events" />
+      <TabIcon imgName="dashboard" name="Feed" />
+      <TabIcon imgName="friends" name="My Networks" />
+      <TabIcon imgName="Group" name="Dating" />
+      <TabIcon imgName="marketplace" name="Marketplace" />
+      <TabIcon imgName="portfolio" name="Jobs" />
+      <TabIcon imgName="calendar" name="Events" />
     </div>
   );
 };
